12d038f9874ad8c5a382f9baa6d5144deccd3d24
Bhavesh Chavda <Bsmarty@outlook.com>
2021-05-23 18:34:02 +0530

    NM: ADD-ignore,comments MOD: reply mods to a class

--- Mods/ReplyMods.py
+++ Mods/ReplyMods.py
@@ -1,24 +1,44 @@
 import json
 import random
-import os
 
-help_msg = os.getenv('HELP_MSG')
+#NOTE: fetches quotes from locak quote fil
+#TO-DO: convert this method with API
 with open("quotes.json","r") as f:
     d = f.read()
 data = json.loads(d)
 
-def replyQuote(message):
-    quote = data[random.randint(0,1840)]["text"]
-    message.reply_message(quote)
+#NOTE: this is used get UserConfig data from UserConfig.json 
+with open("UserConfig.json","r") as f:
+    d = f.read()
+config_data = json.loads(d)
 
-def replyHelp(message):
-    message.reply_message(f"Lavde abhi kam chal rha he apna kam kr nalla gandu!!!!\n\n{help_msg}")
+help_msg = config_data.get("HELP_MSG")
 
-def replyNoods(message):
-    message.reply_message("Tharki sala porn dekh bathroom me jake<><>")
+#NOTE: This is the class where every mods is store you can add you mods as function/method here accordingly...
+#pass thr message object to every new mod
+#NOTE:DON'T forget to add mod descrpition as comment to help other devs
+class replymods:
+    # def __init__(self,name="Chanka"):
+    #     self.message = name
 
-def replyMenu(message):
-    message.reply_message(help_msg)
+    def replyQuote(message):
+        #sends random quotes
+        quote = data[random.randint(0,1840)]["text"]
+        message.reply_message(quote)
 
-def replyModError(message):
-    message.reply_message(f"{message.content} bhosad ye mod nahi chal rha dusra try kr!!!\n\n{help_msg}")
+    def replyHelp(message):
+        #sends help menu
+        message.reply_message(f"Lavde abhi kam chal rha he apna kam kr nalla gandu!!!!\n\n{help_msg}")
+        
+    def replyNoods(message):
+        #sends random nudes
+        message.reply_message("Tharki sala porn dekh bathroom me jake<><>")
+
+    def replyMenu(message):
+        #sends menu
+        message.reply_message(help_msg)
+
+    def replyModError(message):
+        #sends Error msg with menu
+        message.reply_message(f"{message.content} bhosad ye mod nahi chal rha dusra try kr!!!\n\n{help_msg}")
+
--- test-bot.py
+++ test-bot.py
@@ -1,35 +1,48 @@
+################################################################
+# Chanka Bot
+# ----------------------------------------------------------------
+# uses openwa library
+
+
 from openwa import WhatsAPIDriver
-import time
 import json
-import random
-import os
-from Mods import ReplyMods as RM
+from Mods.ReplyMods import replymods as RM  #NOTE:all the mods are saved in this module you can ad new modules there
 
 
-# random.randint(0,22)
-# import asyncio
-# from selenium import webdriver
+#
 # with open("quotes.json","r") as f:
 #     d = f.read()
 # data = json.loads(d)
 
+#NOTE: this is used get UserConfig data from UserConfig.json 
+with open("UserConfig.json","r") as f:
+    d = f.read()
+config_data = json.loads(d)
+help_msg = config_data.get("HELP_MSG")
+prefix = config_data.get("PREFIX")
+
+
+#NOTE:This inisitaes the driver from whatsapp web app and authenictes by scaning
 driver = WhatsAPIDriver(client="chrome",executable_path="chromedriver.exe",headless=False)
 
-prefix = os.getenv("PREFIX")
-help_msg = os.getenv("HELP_MSG")
 
-
+#NOTE:this class is used for receving and handling message
 class NewMessageObserver:
     def on_message_received(self, new_messages):
         for message in new_messages:
             print(f"New message {message} received from number {message.sender.id}")
+
             if message.content.startswith(prefix):
                 if message.content == f"{prefix}quote":
                     RM.replyQuote(message)
                 elif message.content == f"{prefix}help":
+                    print("help start")
                     RM.replyHelp(message)
+                    print("help end")
                 elif message.content == f"{prefix}noods":
+                    print("help start")
                     RM.replyNoods(message)
+                    print("help end")
                 elif message.content == f"{prefix}menu":
                     RM.replyMenu(message)
                 else:
@@ -37,21 +50,12 @@ class NewMessageObserver:
             else:
                 pass
 
-                
-                
-
-# options = webdriver.ChromeOptions()
-# options.add_experimental_option('excludeSwitches', ['enable-logging'])
-# browser = webdriver.Firefox()
-# browser.get('http://selenium.dev/')
+#NOTE:this is used to subscribe message handling class to the driver
 driver.subscribe_new_messages(NewMessageObserver())
+
+#NOTE: loop is infinite coz of continues listning of messages
 while(True):
     pass
-    # # Save image to file
-    # driver.get_qr()
-
-    # # Get image in base64 
-    # driver.get_qr_base64()
 
 
 
